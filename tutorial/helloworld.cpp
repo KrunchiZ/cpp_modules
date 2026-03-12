d6c32a77805aa9bec80a58b7ff8d00ebb9ea5453
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2026-03-12 15:04:30 +0800

    updated helloworld (initializer study)

--- tutorial/helloworld.cpp
+++ tutorial/helloworld.cpp
@@ -6,15 +6,21 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 13:31:34 by kchiang           #+#    #+#             */
-/*   Updated: 2026/03/12 14:30:15 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/03/12 15:03:44 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+// copy initializer using =
+// direct initializer using ()
+// list initializer using {} (similar to compound initializer but without the '=' )
+// copy-list initializer using = {} similar to compound initializer
 int	main(void)
 {
-	std::string str ("Hello world!");
-	std::cout << str;
+	int			nb = 42;
+	std::string	str ("Hello world!");
+
+	std::cout << nb << str;
 	return (0);
 }
