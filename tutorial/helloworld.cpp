0da1a298e18583aff5321320a34168f0ccfb65ed
Kai Zhi Chiang <kchiang@u81z08s03.42kl.edu.my>
2026-03-18 17:53:28 +0800

    progress 2.9 namespaces

--- tutorial/helloworld.cpp
+++ tutorial/helloworld.cpp
@@ -6,11 +6,12 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 13:31:34 by kchiang           #+#    #+#             */
-/*   Updated: 2026/03/13 16:11:11 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/03/18 16:29:44 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 
 // copy initializer using =
 // direct initializer using ()
@@ -25,5 +26,5 @@ int	main(void)
 	std::cout << "Enter three numbers: ";
 	std::cin >> a >> b >> c;
 	std::cout << "You entered " << a << ", " << b << ", and " << c << ".\n";
-	return (0);
+	return (EXIT_SUCCESS);
 }
