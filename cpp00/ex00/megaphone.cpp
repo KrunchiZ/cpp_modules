95a47a36bd5ace05a32bf585d0c832cf415b5e22
Kai Zhi Chiang <kchiang@u80z02s03.42kl.edu.my>
2026-03-27 12:26:02 +0800

    finished cpp00_ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 16:14:11 by kchiang           #+#    #+#             */
-/*   Updated: 2026/03/19 16:44:46 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/03/27 12:22:20 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,17 @@
 
 int	main(int argc, char **argv)
 {
+	using std::cout;
+	if (argc == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < argc; ++i)
+	{
+		for (int j = 0; argv[i][j]; ++j)
+			cout << static_cast<char>(std::toupper(argv[i][j]));
+	}
+	cout << "\n";
+	return (0);
 }
