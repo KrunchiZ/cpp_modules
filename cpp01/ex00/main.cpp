afa9845fc63194c75a0cb22f86c7d6c8d90af9a1
Kai Zhi Chiang <kchiang@u91z01s06.42kl.edu.my>
2026-04-26 22:13:53 +0800

    Improved verbosity in main

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 21:49:07 by kchiang           #+#    #+#             */
-/*   Updated: 2026/04/26 21:51:16 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/04/26 22:12:25 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 
 int	main(void)
 {
+	std::cout << "--- 'Zombie* foo = newZombie(\"foo\");' in main ---\n";
 	Zombie* foo = newZombie("foo");
+	std::cout << '\n';
 
+	std::cout << "--- 'randomChump(\"chump\");' in main ---\n";
 	randomChump("chump");
+	std::cout << '\n';
+
+	std::cout << "--- 'foo->announce();' in main ---\n";
 	foo->announce();
+	std::cout << '\n';
+
+	std::cout << "--- 'delete foo;' in main ---\n";
 	delete foo;
 	return (0);
 }
