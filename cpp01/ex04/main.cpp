e036b7d1d0ebbdd36b9db27d98f8a2501d94cd88
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-04-28 00:20:12 +0800

    Finished ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,53 +6,82 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:23:45 by kchiang           #+#    #+#             */
-/*   Updated: 2026/04/27 20:39:11 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/04/28 00:17:03 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
-#include <cstdlib>
 #include <string>
-#include <sstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::string;
 
 namespace
 {
-	bool	argsIsInvalid(int& argc, char* file);
+bool	argsIsInvalid(int& argc, char* file);
+void	parseLine(ifstream& inFile, ofstream& outFile,
+			const string& s1, const string& s2);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argsIsInvalid(argc, argv[1]))
-		return (EXIT_FAILURE);
+		return (1);
 
-	std::string fileName(argv[1]);
-	std::fstream inFile(fileName);
-	std::fstream outFile(inFile + ".replace", outFile.out | outFile.trunc);
+	ifstream	inFile(argv[1]);
+	ofstream	outFile((string(argv[1]) + ".replace").c_str());
+
 	if (!inFile.is_open() || !outFile.is_open())
 	{
 		std::cerr << "Error: Failed to open file\n";
-		return ;
+		return (1);
 	}
-	for (std::string line(""); line.empty(); )
-	{
-	}
+
+	const string	s1(argv[2]);
+	const string	s2(argv[3]);
+
+	parseLine(inFile, outFile, s1, s2);
+	inFile.close();
+	outFile.close();
+	return (0);
 }
 
 namespace
 {
-	bool	argsIsInvalid(int& argc, char* file)
+bool	argsIsInvalid(int& argc, char* file)
+{
+	if (argc != 4)
 	{
-		if (argc != 4)
-		{
-			std::cerr << "Error: ex04 <infile> <old string> <new string>\n";
-			return (true);
-		}
-		if (std::string(file).empty())
-		{
-			std::cerr << "Error: <infile> cannot be empty string\n";
-			return (true);
-		}
-		return (false);
+		std::cerr << "Error: ex04 <infile> <old string> <new string>\n";
+		return (true);
 	}
+	if (string(file).empty())
+	{
+		std::cerr << "Error: <infile> cannot be empty string\n";
+		return (true);
+	}
+	return (false);
+}
+
+void	parseLine(ifstream& inFile, ofstream& outFile,
+			const string& s1, const string& s2)
+{
+	string	line;
+
+	while (std::getline(inFile, line))
+	{
+		size_t pos = line.find(s1, 0);
+		while (pos != string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+			pos = line.find(s1, pos);
+		}
+		outFile << line;
+		line.clear();
+	}
+}
 }
