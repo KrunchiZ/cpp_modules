2f39d2d8b0e393b7952cfc3c184214050e0c0683
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-05-06 02:07:24 +0800

    finished cpp02ex01. Applied roundf to float constructor to handle int truncation during rawBit conversion

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,12 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 20:53:23 by kchiang           #+#    #+#             */
-/*   Updated: 2026/05/05 22:59:57 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/05/06 02:04:23 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.h"
 #include <iostream>
+#include <cmath>
 
 const int	Fixed::s_fracBits = 8;
 
@@ -19,6 +20,18 @@ Fixed::Fixed()
 	: m_rawBits(0)
 {
 	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const int num)
+	: m_rawBits(num << s_fracBits)
+{
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float num)
+	: m_rawBits(roundf(num * (1 << s_fracBits)))
+{
+	std::cout << "Float constructor called\n";
 }
 
 Fixed::Fixed(const Fixed& other)
@@ -32,7 +45,7 @@ Fixed::~Fixed() {std::cout << "Destructor called\n";}
 Fixed&	Fixed::operator=(const Fixed& rhs)
 {
 	std::cout << "Copy assignment operator called\n";
-	this->m_rawBits = rhs.getRawBits();
+	this->m_rawBits = rhs.m_rawBits;
 	return (*this);
 }
 
@@ -46,4 +59,26 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called\n";
 	m_rawBits = raw;
+}
+
+float	Fixed::toFloat() const
+{
+	return (static_cast<float>(m_rawBits) / (1 << s_fracBits));
+}
+
+int		Fixed::toInt() const
+{
+	return (m_rawBits >> s_fracBits);
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/* Non-member Operator Function Overload                                      */
+/*                                                                            */
+/* ************************************************************************** */
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& fixed)
+{
+	out << fixed.toFloat();
+	return (out);
 }
--- cpp02/ex01/Fixed.h
+++ cpp02/ex01/Fixed.h
@@ -6,14 +6,18 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 19:28:01 by kchiang           #+#    #+#             */
-/*   Updated: 2026/05/05 22:54:15 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/05/06 01:07:07 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 class Fixed
 {
 public:
 	Fixed();
+	Fixed(const int num);
+	Fixed(const float num);
 	Fixed(const Fixed& other);
 	~Fixed();
 
@@ -21,9 +25,13 @@ public:
 	
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
 
 private:
 	static const int	s_fracBits;
 	
 	int	m_rawBits;
 };
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& fixed);
