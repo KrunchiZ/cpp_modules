52423260aaef2072f933d500b52cce5f0205d5bb
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-05-06 21:54:22 +0800

    CPP02exo3: added few more test cases in main and organized the test codes better

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 22:55:08 by kchiang           #+#    #+#             */
-/*   Updated: 2026/05/06 18:14:37 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/05/06 21:51:24 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,57 @@
 
 bool	bsp(const Point a, const Point b, const Point c, const Point point);
 
+namespace
+{
+void	printYes(const Point& a, const Point& b, const Point& c,
+			const Point& point);
+void	printNo(const Point& a, const Point& b, const Point& c,
+			const Point& point);
+}
+
 int	main(void)
 {
 	const Point	a;
 	const Point	b(1.0f, 2.0f);
 	const Point c(3.0f, 0.0f);
-	const Point point(1.0f, 0.0f);
 
-	if (bsp(a, b, c, point))
-		std::cout << point << " is within triangle [" << a << ", " << b
-			<< ", " << c << "]\n";
-	else
-		std::cout << point << " is not within triangle [" << a << ", " << b
-			<< ", " << c << "]\n";
-	
+	{
+		const Point point(1.0f, 0.0f);
+		if (bsp(a, b, c, point))
+			printYes(a, b, c, point);
+		else
+			printNo(a, b, c, point);
+	}
+	{
+		const Point point(1.5f, 0.5f);
+		if (bsp(a, b, c, point))
+			printYes(a, b, c, point);
+		else
+			printNo(a, b, c, point);
+	}
+	{
+		const Point point(2.0f, -0.1f);
+		if (bsp(a, b, c, point))
+			printYes(a, b, c, point);
+		else
+			printNo(a, b, c, point);
+	}
 	return (0);
+}
+
+namespace
+{
+void	printYes(const Point& a, const Point& b, const Point& c,
+			const Point& point)
+{
+	std::cout << point << " is within triangle [" << a << ", " << b
+		<< ", " << c << "]\n";
+}
+
+void	printNo(const Point& a, const Point& b, const Point& c,
+			const Point& point)
+{
+	std::cout << point << " is not within triangle [" << a << ", " << b
+		<< ", " << c << "]\n";
+}
 }
