781fde3bc1b32882c7fe5befc8130ce411dc1e95
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-05-06 17:34:45 +0800

    Finished implementation of bsp.cpp in cpp02ex03

---
+++ cpp02/ex03/bsp.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 17:08:32 by kchiang           #+#    #+#             */
+/*   Updated: 2026/05/06 17:33:28 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.h"
+#include "Fixed.h"
+#include <cmath>
+
+namespace
+{
+Fixed	calculateArea(const Point p1, const Point p2, const Point p3)
+{
+	Fixed	sum1(p1.getX() * p2.getY() + p2.getX() * p3.getY()
+				+ p3.getX() * p1.getY());
+	Fixed	sum2(p1.getY() * p2.getX() + p2.getY() * p3.getX()
+				+ p3.getY() * p1.getX());
+
+	if (sum2 > sum1)
+		return ((sum2 - sum1) / Fixed(2));
+	return ((sum1 - sum2) / Fixed(2));
+}
+}
+
+bool	bsp(const Point a, const Point b, const Point c, const Point point)
+{
+	const Fixed	triangleArea(calculateArea(a, b, c));
+	const Fixed	areaABP(calculateArea(a, b, point));
+	const Fixed	areaACP(calculateArea(a, c, point));
+	const Fixed	areaBCP(calculateArea(b, c, point));
+
+	return (areaABP + areaACP + areaBCP == triangleArea);
+}
