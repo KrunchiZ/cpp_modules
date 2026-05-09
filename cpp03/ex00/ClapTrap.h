252362c183621fafef5ef7ebfb79517f1670ae61
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2026-05-10 01:26:10 +0800

    cpp03ex00: Finished ClapTrap implementation. Pending testing

---
+++ cpp03/ex00/ClapTrap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/10 00:26:14 by kchiang           #+#    #+#             */
+/*   Updated: 2026/05/10 01:22:37 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class ClapTrap
+{
+public:
+	ClapTrap(const std::string& name = "Foo");
+	ClapTrap(const ClapTrap& other);
+	~ClapTrap();
+
+	ClapTrap&	operator=(const ClapTrap& rhs);
+
+	void	attack(const std::string& target = "itself");
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+private:
+	std::string		m_name;
+	unsigned int	m_hitPoints;
+	unsigned int	m_energyPoints;
+	unsigned int	m_attackDamage;
+};
