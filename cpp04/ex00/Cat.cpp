b4eedc8f74df0116badf856bf57e14e077e31d5e
kchiang <kaizhi3006@gmail.com>
2026-07-18 16:42:34 +0800

    feat(cpp04): added Cat class

--- cpp04/ex00/Cat.cpp
+++ cpp04/ex00/Cat.cpp
@@ -1,36 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 15:57:45 by kchiang           #+#    #+#             */
-/*   Updated: 2026/07/18 16:41:07 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/07/18 16:42:11 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.h"
-#include <string>
+#include "Cat.h"
 #include <iostream>
 
-Animal::Animal(): type("Base")
+Cat::Cat(): Animal()
 {
+	type = "Cat";
 	std::cout << "Animal of type " << type << " has spawned.\n";
 }
 
-Animal::Animal(const Animal& other) {*this = other;}
+Cat::Cat(const Cat& other) {*this = other;}
 
-Animal::~Animal()
+Cat::~Cat()
 {
 	std::cout << "Animal of type " << type << " has been destroyed.\n";
 }
 
-Animal&	Animal::operator=(const Animal& rhs)
+Cat& Cat::operator=(const Cat& rhs)
 {
 	if (this != &rhs)
+	{
+		Animal::operator=(rhs);
 		type = rhs.type;
+	}
 	return (*this);
 }
 
-void	Animal::makeSound() const {}
+void	Cat::makeSound() const
+{
+	std::cout << "Meow!\n";
+}
