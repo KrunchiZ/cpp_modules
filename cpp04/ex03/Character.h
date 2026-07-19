6f205d91018a32b3a11b98c4e8d9cc46d03a1aa9
kchiang <kaizhi3006@gmail.com>
2026-07-19 18:11:29 +0800

    feat(cpp04): Added character.h WIP

---
+++ cpp04/ex03/Character.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/18 20:48:54 by kchiang           #+#    #+#             */
+/*   Updated: 2026/07/19 18:11:01 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "AMateria.h"
+#include "ICharacter.h"
+#include <string>
+
+class Character
+{
+public:
+	Character(const std::string& name);
+	Character(const Character& other);
+	Character& operator=(const Character& rhs);
+	virtual ~Character() {}
+
+	virtual const std::string& getName() const;
+	virtual void equip(AMateria* m);
+	virtual void unequip(int idx);
+	virtual void use(int idx, ICharacter& target);
+
+private:
+	std::string	m_name;
+	AMateria*	m_inventory[4];
+	int			m_inventoryCount;
+
+	Character();
+};
+
+#endif
