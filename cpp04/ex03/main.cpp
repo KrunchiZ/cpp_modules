49f58df7d22eb24e4fd558cee066c6df8d981bf3
kchiang <kaizhi3006@gmail.com>
2026-07-21 22:30:45 +0800

    chore(cpp04): added more test in ex03

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 17:20:55 by kchiang           #+#    #+#             */
-/*   Updated: 2026/07/20 18:31:55 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/07/21 22:29:30 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,16 @@ int main()
 
 	me->use(0, *bob);
 	me->use(1, *bob);
+	
+	me->unequip(1);
+	bob->equip(tmp);
+	bob->use(0, *me);
+	bob->unequip(0);
 
 	delete bob;
 	delete me;
 	delete src;
+	delete tmp;
 
 	return 0;
 }
