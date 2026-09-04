2e0e74f29ac01994881ae68738f62fb2b3fda146
kchiang <kaizhi3006@gmail.com>
2026-09-04 16:38:17 +0800

    feat(cpp06): finished ex00 ScalarConverter first version. To be tested.

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/03 13:08:12 by kchiang           #+#    #+#             */
-/*   Updated: 2026/09/03 18:21:30 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/09/04 16:36:34 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,66 @@
 #include <cmath>
 #include <cstdlib>
 #include <cerrno>
+
+namespace
+{
+	void str_tolower(std::string& str);
+	bool isChar(const std::string& input);
+	bool isInt(const std::string& input);
+	bool isFloat(const std::string& input);
+	bool isDouble(const std::string& input);
+	bool isSpecialFloat(const std::string& input);
+	void process_int(const std::string& input);
+	void process_float(const std::string& input);
+	void process_double(const std::string& input);
+	void print_char(int ch);	
+	void print_float(double value);
+	void print_double(long double value);	
+}
+
+ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter(const ScalarConverter&) {}
+ScalarConverter::~ScalarConverter() {}
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter&) {return *this;}
+
+void ScalarConverter::convert(std::string input)
+{
+	str_tolower(input);
+	if (isSpecialFloat(input))
+	{
+		std::cout << "char: impossible\nint: impossible\n";
+		if (input == "nanf")
+			print_float(std::numeric_limits<float>::quiet_NaN());
+		else if (input == "+inff")
+			print_float(std::numeric_limits<float>::infinity());
+		else if (input == "-inff")
+			print_float(-std::numeric_limits<float>::infinity());
+	}
+	else if (isSpecialDouble(input))
+	{
+		std::cout << "char: impossible\nint: impossible\n";
+		if (input == "nan")
+			print_double(std::numeric_limits<double>::quiet_NaN());
+		else if (input == "+inf")
+			print_double(std::numeric_limits<double>::infinity());
+		else if (input == "-inf")
+			print_double(-std::numeric_limits<double>::infinity());
+	}
+	else if (isChar(input))
+	{
+		char ch = input[1];
+		print_char(ch);
+		std::cout << "int: " << static_cast<int>(ch) << "\n";
+		print_float(static_cast<float>(ch));
+		print_double(static_cast<double>(ch));
+	}
+	else if (isInt(input))
+		process_int(input);
+	else if (isFloat(input))
+		process_float(input);
+	else if (isDouble(input))
+		process_double(input);
+}
 
 namespace
 {
@@ -101,6 +161,66 @@ namespace
 	{
 		return (input == "nan" || input == "+inf" || input == "-inf");
 	}
+		
+	void process_int(const std::string& input)
+	{
+		errno = 0;
+		long intNum = std::strtol(input.c_str(), NULL, 10);
+		if (errno == ERANGE
+			|| (intNum < std::numeric_limits<int>::min() || intNum > std::numeric_limits<int>::max()))
+			std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
+		else
+		{
+			print_char(static_cast<int>(intNum));
+			std::cout << "int: " << static_cast<int>(intNum) << "\n";
+			if (intNum < -std::numeric_limits<float>::max() || intNum > std::numeric_limits<float>::max())
+				std::cout << "float: impossible\n";
+			else
+				print_float(static_cast<double>(intNum));
+			if (intNum < -std::numeric_limits<double>::max() || intNum > std::numeric_limits<double>::max())
+				std::cout << "double: impossible\n";
+			else
+				print_double(static_cast<long double>(intNum));
+		}
+	}
+
+	void process_float(const std::string& input)
+	{
+		errno = 0;
+		double floatNum = std::strtod(input.c_str(), NULL);
+		if (errno == ERANGE
+			|| (floatNum && std::abs(floatNum) > std::numeric_limits<float>::max()))
+			std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
+		else
+		{
+			print_char(static_cast<int>(floatNum));
+			if (floatNum < std::numeric_limits<int>::min() || floatNum > std::numeric_limits<int>::max())
+				std::cout << "int: impossible\n";
+			else
+				std::cout << "int: " << static_cast<int>(floatNum) << "\n";
+			print_float(floatNum);
+			print_double(static_cast<long double>(floatNum));
+		}
+	}
+
+	void process_double(const std::string& input)
+	{
+		errno = 0;
+		long double doubleNum = std::strtold(input.c_str(), NULL);
+		if (errno == ERANGE
+			|| (doubleNum && std::abs(doubleNum) > std::numeric_limits<double>::max()))
+			std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
+		else
+		{
+			print_char(static_cast<int>(doubleNum));
+			if (doubleNum < std::numeric_limits<int>::min() || doubleNum > std::numeric_limits<int>::max())
+				std::cout << "int: impossible\n";
+			else
+				std::cout << "int: " << static_cast<int>(doubleNum) << "\n";
+			print_float(static_cast<double>(doubleNum));
+			print_double(doubleNum);
+		}
+	}
 
 	void print_char(int ch)
 	{
@@ -115,8 +235,13 @@ namespace
 			std::cout << "char: Non displayable\n";
 		}
 		
-	void print_float(float value)
+	void print_float(double value)
 	{
+		if (value < -std::numeric_limits<float>::max() || value > std::numeric_limits<float>::max())
+		{
+			std::cout << "float: impossible\n";
+			return;
+		}
 		if (std::isinf(value))
 		{
 			if (std::signbit(value))
@@ -129,14 +254,19 @@ namespace
 		else
 		{
 			if (value == std::floor(value))
-				std::cout << "float: " << value << ".0f\n";
+				std::cout << "float: " << static_cast<float>(value) << ".0f\n";
 			else
-				std::cout << "float: " << value << "f\n";
+				std::cout << "float: " << static_cast<float>(value) << "f\n";
 		}
 	}
 
-	void print_double(double value)
+	void print_double(long double value)
 	{
+		if (value < -std::numeric_limits<double>::max() || value > std::numeric_limits<double>::max())
+		{
+			std::cout << "double: impossible\n";
+			return;
+		}
 		if (std::isinf(value))
 		{
 			if (std::signbit(value))
@@ -149,78 +279,9 @@ namespace
 		else
 		{
 			if (value == std::floor(value))
-				std::cout << "double: " << value << ".0\n";
-			else
-				std::cout << "double: " << value << "\n";
-		}
-	}
-		
-	void print_value(long value)
-	{
-		if (value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max())
-			std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
-		else
-		{
-			print_char(static_cast<int>(value));
-			std::cout << "int: " << static_cast<int>(value) << "\n";
-			if (value && std::abs(value) > std::numeric_limits<float>::max())
-				std::cout << "float: impossible\n";
-			else
-				std::cout << "float: " << static_cast<float>(value) << ".0f\n";
-			if (value && std::abs(value) > std::numeric_limits<double>::max())
-				std::cout << "double: impossible\n";
-			else
 				std::cout << "double: " << static_cast<double>(value) << ".0\n";
+			else
+				std::cout << "double: " << static_cast<double>(value) << "\n";
 		}
-	}
-
-	void print_value(float value)
-	{
-	}
-}
-
-ScalarConverter::ScalarConverter() {}
-ScalarConverter::ScalarConverter(const ScalarConverter&) {}
-ScalarConverter::~ScalarConverter() {}
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter&) {return *this;}
-
-void ScalarConverter::convert(std::string input)
-{
-	str_tolower(input);
-	if (isSpecialFloat(input))
-	{
-		std::cout << "char: impossible\nint: impossible\n";
-		if (input == "nanf")
-			print_float(std::numeric_limits<float>::quiet_NaN());
-		else if (input == "+inff")
-			print_float(std::numeric_limits<float>::infinity());
-		else if (input == "-inff")
-			print_float(-std::numeric_limits<float>::infinity());
-	}
-	else if (isSpecialDouble(input))
-	{
-		std::cout << "char: impossible\nint: impossible\n";
-		if (input == "nan")
-			print_double(std::numeric_limits<double>::quiet_NaN());
-		else if (input == "+inf")
-			print_double(std::numeric_limits<double>::infinity());
-		else if (input == "-inf")
-			print_double(-std::numeric_limits<double>::infinity());
-	}
-	if (isChar(input))
-	{
-		char ch = input[1];
-		print_char(ch);
-		std::cout << "int: " << static_cast<int>(ch) << "\n";
-		print_float(static_cast<float>(ch));
-		print_double(static_cast<double>(ch));
-	}
-	if (isInt(input))
-	{
-		errno = 0;
-		long intNum = std::strtol(input.c_str(), NULL, 10);
-		if (errno == ERANGE)
-			intNum = std::numeric_limits<long>::max();
-		print_value(intNum);
 	}
 }
