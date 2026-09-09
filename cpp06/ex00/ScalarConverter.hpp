0d7882e951c11aa2cacc87e5bcb091b397aa04ab
kchiang <kaizhi3006@gmail.com>
2026-09-09 18:13:54 +0800

    chore(cpp06): add missing define in scarlarconverter

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,12 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 14:55:09 by kchiang           #+#    #+#             */
-/*   Updated: 2026/09/03 16:40:02 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/09/09 18:13:25 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
 #include <string>
 
