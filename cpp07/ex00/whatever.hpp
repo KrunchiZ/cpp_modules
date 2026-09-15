b11f2f2a33f377e804843b38a28b2ec314817556
kchiang <kaizhi3006@gmail.com>
2026-09-16 01:35:15 +0800

    chore(cpp07): added missing const  keyword in some functions in ex00 and ex01

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 14:24:42 by kchiang           #+#    #+#             */
-/*   Updated: 2026/09/11 15:46:16 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/09/16 01:29:57 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ void	swap(T& a, T& b)
 }
 
 template <typename T>
-T	min(T a, T b)
+const T&	min(const T& a, const T& b)
 {
 	return ((a <= b) ? a : b);
 }
 
 template <typename T>
-T	max(T a, T b)
+const T&	max(const T& a, const T& b)
 {
 	return ((a >= b) ? a : b);
 }
--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:32:12 by kchiang           #+#    #+#             */
-/*   Updated: 2026/09/11 16:18:46 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/09/16 01:30:34 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@
 #include <cstddef>
 
 template <typename T, typename Function>
-void	iter(T& array, const size_t size, Function fx)
+void	iter(T& array, const size_t size, const Function fx)
 {
 	if (!size || !fx)
 		return ;
 	for (size_t i = 0; i < size; ++i)
-	{
 		fx(array[i]);
-	}
 }
 
 #endif
